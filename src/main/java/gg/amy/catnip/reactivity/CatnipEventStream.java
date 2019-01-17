60407651755fdaa64367ac36609acdfb8c366871
amy null <null@amy.gg>
2019-01-16 19:08:43 -0500

    Initial commit

---
+++ src/main/java/gg/amy/catnip/reactivity/CatnipEventStream.java
@@ -0,0 +1,96 @@
+package gg.amy.catnip.reactivity;
+
+import com.mewna.catnip.shard.EventType;
+import io.vertx.core.Handler;
+import io.vertx.core.streams.ReadStream;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.annotation.Nonnull;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author amy
+ * @since 1/16/19.
+ */
+@Accessors(fluent = true)
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CatnipEventStream<T> implements ReadStream<T> {
+    @Getter
+    private final EventType<T> type;
+    private final Deque<T> buffer = new LinkedList<>();
+    
+    private boolean paused;
+    
+    private Handler<T> handler;
+    private Handler<Throwable> exceptionHandler;
+    
+    public static <E> CatnipEventStream<E> create(final EventType<E> type) {
+        return new CatnipEventStream<>(type);
+    }
+    
+    public void write(@Nonnull final T event) {
+        try {
+            if(paused) {
+                buffer.addLast(event);
+            } else {
+                handler.handle(event);
+            }
+        } catch(final Exception e) {
+            exceptionHandler.handle(e);
+        }
+    }
+    
+    public ReadStream<T> exceptionHandler(final Handler<Throwable> handler) {
+        exceptionHandler = handler;
+        return this;
+    }
+    
+    public ReadStream<T> handler(final Handler<T> handler) {
+        this.handler = handler;
+        return this;
+    }
+    
+    public ReadStream<T> pause() {
+        paused = true;
+        return this;
+    }
+    
+    public ReadStream<T> resume() {
+        paused = false;
+        try {
+            if(!buffer.isEmpty()) {
+                // Assuming that they might update pause-state from another thread
+                while(!buffer.isEmpty() && !paused) {
+                    handler.handle(buffer.pop());
+                }
+            }
+        } catch(final Exception e) {
+            exceptionHandler.handle(e);
+        }
+        return this;
+    }
+    
+    public ReadStream<T> fetch(final long l) {
+        long counter = l;
+        try {
+            while(!buffer.isEmpty() && counter > 0) {
+                handler.handle(buffer.pop());
+                --counter;
+            }
+        } catch(final Exception e) {
+            exceptionHandler.handle(e);
+        }
+        return this;
+    }
+    
+    public ReadStream<T> endHandler(final Handler<Void> handler) {
+        // We don't bother w/ this since the stream doesn't end unless the
+        // bot is stopped entirely, which generally just means that the
+        // application as a whole has stopped.
+        return this;
+    }
+}
---
+++ src/main/java/gg/amy/catnip/reactivity/rxjava2/RxJava2Extension.java
@@ -0,0 +1,24 @@
+package gg.amy.catnip.reactivity.rxjava2;
+
+import com.mewna.catnip.extension.AbstractExtension;
+import com.mewna.catnip.shard.EventType;
+import gg.amy.catnip.reactivity.CatnipEventStream;
+import io.reactivex.Observable;
+import io.vertx.reactivex.ObservableHelper;
+
+/**
+ * @author amy
+ * @since 1/16/19.
+ */
+@SuppressWarnings("unused")
+public class RxJava2Extension extends AbstractExtension {
+    public RxJava2Extension() {
+        super("rx-java-2");
+    }
+    
+    public <T> Observable<T> createObservable(final EventType<T> type) {
+        final CatnipEventStream<T> stream = CatnipEventStream.create(type);
+        on(type, stream::write);
+        return ObservableHelper.toObservable(stream);
+    }
+}
